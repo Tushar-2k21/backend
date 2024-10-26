d2d5a5d1173d41e992f67925fc02b3d7c305f67f
Tushar Singh <tusharsingh@Tushars-MacBook-Air.local>
2024-10-26 19:32:40 +0530

    mongodb connections

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// METHOD 2 CONNECTION
+
+import express from "express";
+import connectDB from "./db/index.js";
+
+connectDB()
+
+// ===========================================================================
+
+
+
+/*
+// METHOD 1 to connect with mongo db
+
+import dotenv from "dotenv";
+const app = express();
+dotenv.config({
+  path: "./env",
+});
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+// method 1 : to connect to DB
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    // express check if able to listen
+    app.on("error", () => {
+      console.log("ERR", error);
+      throw error;
+    });
+
+    //express listening
+    app.listen(process.env.PORT, () => {
+      console.log(`SERVER LISTENING ON ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error("ERROR", err);
+  }
+})();
+*/
